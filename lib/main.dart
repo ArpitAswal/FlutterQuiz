56b702ddfdc0f1d91d59b248233d56262c55cc3a
arpitaswal <arpitaswal995@gmail.com>
2023-07-17 21:54:35 +0530

    after cloning code

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:clonequiz/playQuiz.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:websafe_svg/websafe_svg.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,25 +14,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'MCQ',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
         //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        // is not restarted.
+        appBarTheme: const AppBarTheme(elevation: 0),
+        primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -51,75 +49,140 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyHomePage> createState() => MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class MyHomePageState extends State<MyHomePage> {
+  static TextEditingController name= TextEditingController();
+  TextEditingController mail= TextEditingController();
 
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+   name.text='';
+   mail.text='';
+  }
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return WillPopScope(
+    onWillPop: () async{
+      SystemNavigator.pop();
+      return true;
+    },
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: null,
+        body: Container(
+          width: double.infinity,
+          height: double.infinity,
+          color: Colors.black,
+          child: Center(
+            child: Stack(
+              children: [
+                Center(child: WebsafeSvg.asset("assets/bg.svg", fit: BoxFit.cover)),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 70,),
+                    const Padding(
+                      padding: EdgeInsets.only(left: 15),
+                      child: Text("Let's Play Quiz",style: TextStyle(color:Colors.white,fontWeight: FontWeight.bold,fontSize: 35),),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.only(left: 15),
+                      child: Text("Enter your informations below",style: TextStyle(color:Colors.white,fontSize: 15),),
+                    ),
+                    const SizedBox(height: 80,),
+                    Center(
+                      child: SizedBox(width: 350,
+                        child: TextField(
+                          controller: name,
+                          keyboardType: TextInputType.name,
+                          textInputAction: TextInputAction.next,
+                          decoration: const InputDecoration(
+                            filled: true,
+                            fillColor: Colors.white54,
+                            labelText: "Enter your name",
+                            labelStyle: TextStyle(color: Colors.deepOrange,fontSize: 20,fontWeight: FontWeight.w700),
+                            hintText: "Full Name",
+                            enabledBorder:OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(12)),
+                                borderSide:BorderSide(width: 2,color: Colors.black) ),
+                            focusedBorder: OutlineInputBorder(borderSide:
+                            BorderSide(width: 2,color:Colors.blue),
+                                borderRadius: BorderRadius.all(Radius.circular(12))),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height:25),
+                    Center(
+                      child: SizedBox(width: 350,
+                        child: TextField(
+                          controller: mail,
+                          keyboardType: TextInputType.emailAddress,
+                          textInputAction: TextInputAction.next,
+                          decoration: const InputDecoration(
+                              filled: true,
+                              fillColor: Colors.white54,
+                              labelText: "Enter your e-mail",
+                              labelStyle: TextStyle(color: Colors.deepOrange,fontSize: 20,fontWeight: FontWeight.w700),
+                              hintText: "E-Mail",
+                              enabledBorder:OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(12)),
+                                  borderSide:BorderSide(width: 2,color: Colors.black) ),
+                              focusedBorder: OutlineInputBorder(borderSide:
+                              BorderSide(width: 2,color:Colors.blue),
+                                  borderRadius: BorderRadius.all(Radius.circular(12)))
+                          ),
+                        ),
+
+                      ),
+                    ),
+                    const SizedBox(height:25),
+
+                    InkWell(
+                      onTap: (){
+                        debugPrint("${Text(name.text.toString())}");
+                        if(name.text.isEmpty || mail.text.isEmpty){
+                          var snackbar= const SnackBar(content: Text('please filled the both textfields'));
+                          ScaffoldMessenger.of(context).showSnackBar(snackbar);
+                        }
+                        else {
+                          Navigator.pushReplacement(context, MaterialPageRoute(
+                              builder: (context) => const PlayQuiz()),);
+                        }
+                        },
+                      child: Center(
+                        child: Container(
+                          width:350,
+                          alignment: Alignment.center,
+                          padding: const EdgeInsets.all(8), // 15
+                          decoration: const BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: [Color(0xFF46A0AE), Color(0xFF00FFCB)],
+                              begin: Alignment.centerLeft,
+                              end: Alignment.centerRight,
+                            ),
+                            borderRadius: BorderRadius.all(Radius.circular(12)),
+                          ),
+                          child: const Text(
+                              " START ",
+                              style: TextStyle(color: Colors.black54,fontSize: 20,fontWeight: FontWeight.w600)),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 115),
+                    Center(child: Image.asset('assets/quizPicture.jpg',width:370))
+                  ],
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+
+
